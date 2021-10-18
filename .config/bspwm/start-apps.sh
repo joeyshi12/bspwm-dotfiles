1d7c8f75725c1e8cba676c1c7d535e0c005ce948
joey <joeyshi12@gmail.com>
2021-10-17 21:52:15 -0700

    Removed unused app configs

--- .config/bspwm/start-apps.sh
+++ .config/bspwm/start-apps.sh
@@ -10,13 +10,13 @@ xrandr --output HDMI-1-1 --primary --mode 2560x1440 --output DVI-I-1 --mode 1920
 sxhkd -c ~/.config/bspwm/sxhkd/sxhkdrc &
 
 # Polybar
-~/.config/bspwm/polybar/launch.sh --forest
+~/.config/bspwm/polybar/launch.sh
 
 # Restore cursor theme
 xsetroot -cursor_name left_ptr &
 
 # Restore wallpaper
-feh --bg-fill ~/.config/bspwm/wallpapers/botw.jpg
+feh --bg-fill ~/.config/bspwm/wallpapers/aurora.jpg
 
 # Compositor
 /usr/local/bin/picom --experimental-backends --config ~/.config/bspwm/picom/picom.conf
