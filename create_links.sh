db602275102ec309bce16957c63abcd3e57c5e2c
joey <joeyshi12@gmail.com>
2021-10-29 19:08:50 -0700

    removed non-essential font packs + added create_links script

---
+++ create_links.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+dir="$HOME/Workspace/dotfiles"
+
+# Handles creating a link file for $1 targetting $2
+# $1 source path
+# $2 target path
+create_link() {
+    if [[ -e "$2" ]]; then
+        echo "$2 exists. Replace? (y/n)"
+        read ans
+        if [[ $ans == "y" ]]; then
+            rm -r $2
+            echo "deleted $2"
+        else
+            return
+        fi
+    fi
+    ln -s $1 $2
+}
+
+# Link configs
+for item in $(ls $dir/.config); do
+    create_link $dir/.config/$item ~/.config/$item
+done
+
+# Link profile
+create_link $dir/.profile ~/.profile
+
+# Link vimrc
+create_link $dir/.vimrc ~/.vimrc
+
+# Link tmux.conf
+create_link $dir/.tmux.conf ~/.tmux.conf
+
+# Link bashrc
+create_link $dir/.bashrc ~/.bashrc
+create_link $dir/.bash_aliases ~/.bash_aliases
+
