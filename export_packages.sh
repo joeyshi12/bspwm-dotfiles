9c9028855e255e0e4ff1952732ca7ff03c76c424
Joey <joeyshi12@gmail.com>
2022-01-25 20:44:33 -0800

    updated

--- export_packages.sh
+++ export_packages.sh
@@ -3,8 +3,8 @@
 # creates list of all installed packages
 # reinstall with pacman -S $(cat pkglist)
 
-pacman -Qqet | grep -v "$(pacman -Qqm)" > pkglist
+# Export list of native packages
+pacman -Qqen > pkglist
 
-# A list of local packages (includes AUR and locally installed)
-pacman -Qm > pkglocallist
-
+# Export list of foreign packages (includes AUR and locally installed)
+pacman -Qqem > pkglocallist
