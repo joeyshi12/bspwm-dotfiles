7e8f5af19e3725d0cc9a70f51349a2c772928679
Joey <joeyshi12@gmail.com>
2022-03-03 23:57:59 -0800

    fixed network script

--- .config/bspwm/rofi/wifi_menu.sh
+++ .config/bspwm/rofi/wifi_menu.sh
@@ -4,16 +4,19 @@ DIR="$HOME/.config/bspwm/rofi"
 NETWORKS_CACHE="$HOME/.cache/networks"
 
 current_wifi=$(nmcli connection show --active | sed 1,1d | awk '{print $1}')
-choice="$(cat "$NETWORKS_CACHE" | grep -v $current_wifi | rofi -dmenu -p "Networks" -theme $DIR/wifi_menu.rasi)"
-ssid="$(echo $choice | awk '{print $1}')"
+ssid="$(grep -v "$current_wifi" "$NETWORKS_CACHE" | rofi -dmenu -p "Networks" -theme "$DIR/wifi_menu.rasi" | awk '{print $1}')"
 
 if [ ! -z "$ssid" ]; then
     password=$(rofi -i -dmenu -password -no-fixed-num-lines -p "Password: " -theme $DIR/input.rasi)
-    response=$([ -z "$password" ] || nmcli dev wifi connect $ssid password $password)
-    if [ -z "$(echo "$response" | grep "Error")" ]; then
-        notify-send "Successfully connected to $ssid"
+    if [ -z "$password" ]; then
+        response="$(nmcli dev wifi connect $ssid)"
     else
+        response="$(nmcli dev wifi connect $ssid password $password)"
+    fi
+    if [ -z "$(echo "$response" | grep -v "Error")" ]; then
         notify-send "Failed to connect to $ssid"
+    else
+        notify-send "Successfully connected to $ssid"
     fi
 fi
 
