55d5fb62516c4a714bc43e805f185cf8dab7f70c
Joey <joeyshi12@gmail.com>
2022-06-07 05:30:41 -0700

    hmm

--- .config/bspwm/sxhkd/switch_desktops.sh
+++ .config/bspwm/sxhkd/switch_desktops.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 # Switches to the $1-th desktop across each monitor and stays focused on current monitor
+num_desktops_left=$(bspc query -m "HDMI-0" -D | wc -l)
+
 left_target=$1
-right_target=$(($1 + 3))
+right_target=$(($left_target + $num_desktops_left))
 focused_desktop=$(bspc query -D -d focused --names)
 
-if [[ "${focused_desktop}" -gt 3 ]]; then
+if [[ "${focused_desktop}" -gt "${num_desktops_left}" ]]; then
     bspc desktop -f "^${left_target}"
     bspc desktop -f "^${right_target}"
 else
